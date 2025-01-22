52f79c2bbb0d3d7213ceab791966d0beafae731c
Adrian Rangel Martinez <142278016+AdrianRang@users.noreply.github.com>
2025-01-22 13:29:43 -0600

    interactive

--- script.js
+++ script.js
@@ -2,33 +2,69 @@ const machine_profile = "assets/mariposa.png"
 const user_profile = "assets/default.png"
 const sound_icon = "assets/escuchar.png"
 const audio = new Audio("assets/neon.mp3")
+let json;
+fetch('./responses.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        json = data;
+        message(json.messages[0], false)
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
 
-function message(mes, user) {
+
+function message(obj) {
+    let mes = obj.message
     const new_mes = document.getElementById("conversation").appendChild(document.createElement("div"));
     new_mes.classList.add("full-message")
-    const hear = document.createElement("img")
+    let hear;
+    hear = document.createElement("img")
     hear.src = sound_icon;
     hear.classList.add("sound")
     const img = document.createElement("img")
-    img.src = user ? user_profile : machine_profile
+    img.src = machine_profile
     img.className = "profile"
-    const message = document.createElement("div")
-    message.classList.add("message", user ? "user":"nube");
-    message.innerHTML = mes;
+    const new_message = document.createElement("div")
+    new_message.classList.add("message", "nube");
+    new_message.innerHTML = mes;
 
-    if(user) {
-        new_mes.appendChild(message)
-        new_mes.appendChild(img)
-    } else {
-        new_mes.appendChild(img)
-        new_mes.appendChild(message)
-        new_mes.appendChild(hear)
-    }
+    new_mes.appendChild(img)
+    new_mes.appendChild(new_message)
+    new_mes.appendChild(hear)
+
+    let options = obj.responses;
+    console.log("options: " + options)
+    options.forEach(element => {
+        const button = document.createElement("button");
+        button.innerHTML = element.option;
+        button.onclick = () => {
+            document.getElementById("response").innerHTML ="";
+            message_user(element.option);
+            message(json.messages[element.sendTo])    
+        }
+        document.getElementById("response").appendChild(button)
+    });
+    document.getElementById("response").scrollIntoView();
 }
 
-a()
-async function a() {
-    await document.getElementsByClassName("s-button")[0].addEventListener("click", () => {
+function message_user(mes) {
+    const new_mes = document.getElementById("conversation").appendChild(document.createElement("div"));
+    new_mes.classList.add("full-message")
+    const img = document.createElement("img")
+    img.src = user_profile
+    img.className = "profile"
+    const new_message = document.createElement("div")
+    new_message.classList.add("message", "user");
+    new_message.innerHTML = mes;
+
+    new_mes.appendChild(new_message)
+    new_mes.appendChild(img)
+}
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    message(json.messages[0])
+    document.getElementsByClassName("s-button")[0].addEventListener("click", () => {
         audio.play()
     })
-}
+})
